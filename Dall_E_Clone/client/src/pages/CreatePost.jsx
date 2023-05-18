815345789800c4a1fb184996d79511ca6af59ec5
SiDDyy007 <siddhantp0@gmail.com>
2023-05-17 20:55:00 -0700

    Done for the day

--- Dall_E_Clone/client/src/pages/CreatePost.jsx
+++ Dall_E_Clone/client/src/pages/CreatePost.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState, } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets';
+import {getRandomPrompt} from './utils';
+import { FormField, Loader } from '../components';
+
 
 const CreatePost = () => {
+  const navigate = userNavigate();
+  const [form, setForm] = useState( {
+    name : '', 
+    prompt : '',
+    photo : '',
+
+  });
+
+  const [ ]
   return (
     <div>CreatePost</div>
   )
