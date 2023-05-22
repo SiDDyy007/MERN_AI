198ba6e759028165ac6481d147eeaa246650d389
SiDDyy007 <siddhantp0@gmail.com>
2023-05-22 10:46:59 -0700

    Complete

--- Dall_E_Clone/client/src/pages/Home.jsx
+++ Dall_E_Clone/client/src/pages/Home.jsx
@@ -15,6 +15,47 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method : 'GET',
+          headers : {
+            'Content-Type' : 'application/json'
+          },
+        })
+        if (response.ok){
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+        
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+
+  }, []);
+
+  const handleSearchChange= (e) => {
+    setSearchText(e.target.value);
+    setSearchTimeout(
+    setTimeout(() => {
+      const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+                            item.prompt.toLowerCase().includes(searchText.toLowerCase()) );
+
+      setSearchedResults(searchResults);
+  }, 500) ) };
+
   return (
     <section className='max-w-7x1 mx-auto'>
       <div>
@@ -23,7 +64,7 @@ const Home = () => {
       </div>
 
       <div className='mt-16'>
-        <FormField />
+        <FormField  LabelName="Search Posts" type="text" name="text" placeholder="Search Posts" value={searchText} handleChange={handleSearchChange}/>
       </div>
 
       <div className='mt-10'>
@@ -41,9 +82,9 @@ const Home = () => {
 
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
-                <RenderCards data={[]} title='No search results found'/>
+                <RenderCards data={searchedResults} title='No search results found'/>
               ) : (
-                <RenderCards data={[]} title='No posts found' />
+                <RenderCards data={allPosts} title='No posts found' />
               )}
 
             </div>
